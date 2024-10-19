4c47fdd3406f73a79c91e14f1c5cfba8cc6345ed
Allan Araya Reyes <allanreyes0107@gmail.com>
2024-10-19 04:32:18 -0600

    Fixed database connection

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -4,8 +4,8 @@ const connectMongoDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Mongo connected: ${conn.connection.host}`)
-    }catch (e) {
-        console.log(`Error connecting to MongoDB: ${err.message}`)
+    }catch (err) {
+        console.log(`Error connecting to MongoDB: ${err.message} ${process.env.MONGO_URI}`)
         process.exit(1)
     }
 }
