1a5ced9627a0b3e01c73e3ce42d3bc1211eed859
Allan Araya Reyes <allanreyes0107@gmail.com>
2024-12-13 19:35:07 -0600

    Started the user auth process

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -148,6 +148,7 @@ export const logout = async (req, res) => {
 
 export const getMe = async (req, res) => {
     try {
+
         const user = await User.findById(req.user._id).select("-password");
         console.log(req.user._id)
 
